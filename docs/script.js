90ada5f682d460b092a514222650a127ee3ce976
jgmoon <jgmoon@buptle.com>
2025-07-01 18:24:20 +0900

    파일 위치 수정

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,5 @@
 async function pickRestaurant() {
-  const res = await fetch('./data/random_restaurants.json');
+  const res = await fetch('./random_restaurants.json');
   const data = await res.json();
   const random = data[Math.floor(Math.random() * data.length)];
   document.getElementById('result').innerText = `오늘은 "${random.name}"!`;
